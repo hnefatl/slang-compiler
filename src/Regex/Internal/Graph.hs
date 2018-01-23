15b6337cd687aa542bffb21aa6f18b031943086e
Keith <hnefatl@gmail.com>
2018-01-23 18:21:05 +0000

    Added union, all compiles and type-checks. Need to write some test cases.

--- src/Regex/Internal/Graph.hs
+++ src/Regex/Internal/Graph.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Regex.Internal.Graph where
 
 import qualified Data.Map as M
 import qualified Data.Vector as V
+import Data.List (foldl')
 
 data Node a = Node a deriving Show
 type NodeIndex = Int
@@ -13,14 +16,24 @@ srcNode (s,_,_) = s
 arcInfo :: Edge a -> (a, NodeIndex)
 arcInfo (_,x,d) = (x,d)
 
-data Graph a b = Graph
-    {
-        adjList :: V.Vector (Node a, M.Map b NodeIndex)
-    } deriving Show
+type AdjList a b = V.Vector (Node a, M.Map b NodeIndex)
 
 
-buildGraph :: Ord b => [Node a] -> [Edge b] -> Graph a b
-buildGraph nodes edges = Graph { adjList = V.fromList elements }
-        where elements = zip nodes (map getAdjacent [0..])
+-- Construct a graph from a given list of nodes and list of edges.
+buildGraph :: forall a b. Ord b => [Node a] -> [Edge b] -> AdjList a b
+buildGraph nodes edges = V.fromList elements
+        where elements :: [(Node a, M.Map b NodeIndex)]
+              elements = zip nodes (map getAdjacent [0..])
 
+              getAdjacent :: NodeIndex -> M.Map b NodeIndex
               getAdjacent i = M.fromList $ map arcInfo $ filter ((== i) . srcNode) edges
+
+insertEdge :: Ord b => AdjList a b -> Edge b -> AdjList a b
+insertEdge list (s,x,d) = vectorUpdate (\(n, neighbours) -> (n, M.insert x d neighbours)) s list
+
+insertEdges :: Ord b => AdjList a b -> [Edge b] -> AdjList a b
+insertEdges = foldl' insertEdge
+
+-- Helper function - update a value at a given index in a vector
+vectorUpdate :: (a -> a) -> Int -> V.Vector a -> V.Vector a
+vectorUpdate f i v = v V.// [(i, f $ v V.! i)]
--- src/Regex/Internal/NFA.hs
+++ src/Regex/Internal/NFA.hs
@@ -2,6 +2,8 @@ module Regex.Internal.NFA where
 
 import qualified Regex.Internal.Graph as G
 import qualified Data.Set as S
+import qualified Data.Vector as V
+import qualified Data.Map as M
 
 data NFAInput = Epsilon | Symbol Char deriving (Show, Eq)
 
@@ -13,22 +15,67 @@ instance Ord NFAInput where
 
 data NFA = NFA
     {
-        nodes :: G.Graph () NFAInput, -- In an NFA, we don't have any labels on the nodes
-        start :: G.NodeIndex,
+        graph :: G.AdjList () NFAInput, -- In an NFA, we don't have any labels on the nodes
+        starts :: S.Set G.NodeIndex,
         acceptors :: S.Set G.NodeIndex
     } deriving Show
 
 
--- The empty expression
+-- merge puts the NFAs into the same "address space", and joins them with the given edges.
+-- The first edge list is the list of edges from the 1st NFA to the 2nd, and the 2nd list is the opposite.
+-- This is a helper function for the combination functions below
+mergeWithEdges :: NFA -> [G.Edge NFAInput] -> NFA -> [G.Edge NFAInput] -> NFA
+mergeWithEdges (NFA g1 s1 a1) es12 (NFA g2 s2 a2) es21 =
+    let
+        g1Length = V.length g1
+
+        -- Get the joining edge list, translating the nodes in the 2nd NFA
+        es = (map (\(s,x,d) -> (s,x,d + g1Length)) es12) ++ (map (\(s,x,d) -> (s + g1Length,x,d)) es21)
+
+        -- Shift all the indices in the 2nd NFA's details to the position of its nodes in the appended array        
+        g2' = V.map (\(node, neighbours) -> (node, M.map (+g1Length) neighbours)) g2
+        gr = G.insertEdges (g1 V.++ g2') es -- Add the extra edges
+        st = S.union s1 $ S.map (+g1Length) s2
+        ac = S.union a1 $ S.map (+g1Length) a2
+    in
+        NFA gr st ac
+
+-- Specialisation of mergeWithEdges for when the NFAs are disjoint
+merge :: NFA -> NFA -> NFA
+merge n1 n2 = mergeWithEdges n1 [] n2 []
+
+-- These rules are for combining NFA-e's, as given here: https://en.wikipedia.org/wiki/Thompson%27s_construction#Rules
+
+-- An NFA with 2 nodes and 1 edge, from the start state to an accepting state on the given input
+hop :: NFAInput -> NFA
+hop i = NFA g (S.singleton 0) (S.singleton 1)
+            where g = G.buildGraph [G.Node (), G.Node ()] [(0, i, 1)]
+
+-- Matches the empty input
 empty :: NFA
-empty = NFA { nodes = g, start = 0, acceptors = S.singleton 1 }
-            where g = G.buildGraph [G.Node (), G.Node ()] [(0, Epsilon, 1)]
+empty = hop Epsilon
 
--- The expression of a single symbol
-singleton :: Char -> NFA
-singleton c = NFA { nodes = g, start = 0, acceptors = S.singleton 1 }
-            where g = G.buildGraph [G.Node (), G.Node ()] [(0, Symbol c, 1)]
+-- Matches a single symbol
+single :: Char -> NFA
+single c = hop (Symbol c)
 
--- Union of two expressions
---union :: NFA -> NFA
---union g1 g2
+-- Union of expressions
+union :: [NFA] -> NFA
+union ns =
+    let
+        start = NFA (G.buildGraph [(G.Node ())] []) S.empty S.empty
+        end = NFA (G.buildGraph [(G.Node ())] []) S.empty S.empty
+
+        middle = foldl1 merge ns -- Disjoint merge the given NFAs
+
+        -- Construct edges from the start to every start state in the merged NFA
+        startEdges = map (\i -> (0, Epsilon, i)) (S.toList $ starts middle)
+        endEdges = map (\i -> (i, Epsilon, 0)) (S.toList $ acceptors middle)
+
+        g = mergeWithEdges start startEdges middle [] -- Add the start state
+        g' = mergeWithEdges g [] end endEdges -- Add the accepting state
+
+        startIndex = 0
+        endIndex = V.length (graph g') - 1
+    in
+        NFA (graph g') (S.singleton startIndex) (S.singleton endIndex)
